b2c6d1a57e4769144d96f9584f64d11e589f8ebb
Sean Kembo <kembosean7@gmail.com>
2025-01-04 22:37:41 +0200

    Add list of words to Words class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to HangMan");
+    Words words = new Words();
+
+
+    System.out.println(words.wordsList());
     }
 }
